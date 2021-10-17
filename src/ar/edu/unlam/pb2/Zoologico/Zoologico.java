a6850e1af794898ac177a17592e732e23dc3c65b
LucasLopez03 <lucasaxellopez3@gmail.com>
2021-10-16 22:45:26 -0300

    Primer commit

---
+++ src/ar/edu/unlam/pb2/Zoologico/Zoologico.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.pb2.Zoologico;
+
+public class Zoologico {
+
+}
---
+++ test/ar/edu/unlam/pb2/Zoologico/ZoologicoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.Zoologico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ZoologicoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
